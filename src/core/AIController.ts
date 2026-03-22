b23daeea899672c958f15318a158a37e6d5c7de1
Evgeniy <evgeniyzuev43@gmail.com>
2026-03-22 17:45:37 +0300

    mines lower chance

--- src/core/AIController.ts
+++ src/core/AIController.ts
@@ -174,7 +174,7 @@ export class AIController {
                 // Flee! (Military only flees if alone and overwhelmed, Raiders/Orcs never)
                 const runDir = npc.position.sub(closestThreat.position).normalize();
                 npc.setTarget(npc.position.add(runDir.scale(800)));
-                if (npc.abilities.mine.cooldown <= 0 && Math.random() < 0.2) {
+                if (closestThreat && minDistThreat <= 800 && npc.abilities.mine.cooldown <= 0 && Math.random() < 0.1) {
                     this.game.dropWarpMine(npc);
                 }
                 npc.state = 'flee';
